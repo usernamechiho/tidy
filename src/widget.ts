4517e57ddbd66aa4bb84186471f4d26c9aeb2ddf
이치호 <minepanorama@naver.com>
2025-07-27 20:21:23 +0900

    FEAT: 타입스크립트 추가

---
+++ src/widget.ts
@@ -0,0 +1,30 @@
+(function () {
+  function initializeWidget() {
+    const widget = document.createElement("button");
+
+    widget.className = `fixed
+    bottom-8 right-8 w-16 h-16
+    rounded-full shadow-xl z-10000
+    flex items-center justify-center
+    text-3xl font-bold border border-gray-300
+    transition-transform duration-150 ease-in-out
+    hover:scale-110 active:scale-100
+    `;
+
+    widget.innerHTML = "🐼";
+
+    // 버튼 클릭 이벤트를 추가합니다.
+    widget.addEventListener("click", () => {
+      alert("Tidy 위젯이 클릭되었습니다!");
+    });
+
+    // 완성된 버튼을 페이지의 <body> 태그에 추가합니다.
+    document.body.appendChild(widget);
+  }
+
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", initializeWidget);
+  } else {
+    initializeWidget();
+  }
+})();
